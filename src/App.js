e0a11d6eea7cf28fbd324ec675aa13246298a970
raeoxfordhebron <rae.oxford.hebron@gmail.com>
2023-02-24 16:27:00 -0500

    troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidenav from './components/Sidenav' 
+import Explore from './pages/Explore'
+import Home from './pages/Home'
+import Settings from './pages/Settings'
+import Statistics from './pages/Statistics'
 
 function App() {
   return (
     <div className="App">
+      <Sidenav/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/explore" element={<Explore/>}/>
+          <Route path="/statistics" element={<Statistics/>}/>
+          <Route path="/settings" element={<Settings/>}/>
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,9 +1,19 @@
 import { navData } from "../lib/navData";
+import styles from "./sidenav.module.css"
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft"
 
 export default function Sidenav(){
     return(
         <div>
-            
+            <button className={styles.menuBtn}>
+                <KeyboardDoubleArrowLeftIcon />
+            </button>
+            {navData.map(item => {
+                return <div key={item.id} className={styles.sideItem}>
+                    {item.icon}
+                    <span className={styles.linkText}>{item.text}</span>
+                    </div>
+            })}
         </div>
     )
 }
